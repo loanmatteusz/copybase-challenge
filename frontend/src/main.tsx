5baf6e3d524c0532aed61c209874f9717272e3b6
Loan Matteus <loan.matteusz@gmail.com>
2024-01-24 15:01:12 -0300

    fix: remove dropzone styles import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 
 import '@mantine/core/styles.css';
-import '@mantine/dropzone/styles.css';
 import App from './App.tsx';
 
 import './index.css';
