1ab98234f089ae4bcdf5615e442662620c89f775
Loan Matteus <loan.matteusz@gmail.com>
2024-01-24 02:24:01 -0300

    fix: cors already init in main file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,19 +1,9 @@
-import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { SpreadsheetModule } from './modules/spreadsheet/spreadsheet.module';
-import * as cors from 'cors';
 
 @Module({
   imports: [SpreadsheetModule],
   controllers: [],
   providers: [],
 })
-export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
-    const corsOptions = {
-      origin: 'http://localhost:5173',
-      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-      credentials: true,
-    };
-    consumer.apply(cors(corsOptions)).forRoutes('*');
-  }
-}
+export class AppModule {}
